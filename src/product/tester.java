58fb4fb81ddc1ec7345aee9a73e2c9a664a3a563
mdhenke <38406783+mdhenke@users.noreply.github.com>
2018-09-09 18:20:40 -0600

    Add files via upload

---
+++ src/product/tester.java
@@ -0,0 +1,11 @@
+package product;
+
+public class tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Database d = new Database();
+		Login login = new Login(d);
+	}
+
+}
