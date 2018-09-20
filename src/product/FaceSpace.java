2bd18de6e03be5d2e8d4418fb68c0b4263be9b43
mdhenke <38406783+mdhenke@users.noreply.github.com>
2018-09-19 22:35:27 -0600

    Update FaceSpace.java

--- src/product/FaceSpace.java
+++ src/product/FaceSpace.java
@@ -1,7 +1,14 @@
 package product;
 
+/**
+ * @author marcushenke
+ * This is the driver class for the FaceSpace Application
+ */
 public class FaceSpace {
-
+	/**
+	 * Launches the program
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Database d = new Database();
 		Login login = new Login(d);
